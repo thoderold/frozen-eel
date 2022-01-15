18250389a12b05555cb792488e314b8d306d41e0
OguzhanKesrik <oguzhan.kesrik@gmail.com>
2022-01-15 19:59:24 +0300

    freezing-eel

---
+++ main.py
@@ -0,0 +1,23 @@
+import eel
+import random
+
+eel.init('templates')
+
+
+@eel.expose
+def py_random():
+    return random.random()
+
+
+eel.start('index.html', block=False, size=(400, 300))
+
+# Synchronous calls must happen after start() is called
+
+# Get result returned synchronously by
+# passing nothing in second brackets
+#                   v
+n = eel.js_random()()
+print('Got this from Javascript:', n)
+
+while True:
+    eel.sleep(1.0)
